20df9726deee8bb5a605481ba9144fd35ee45d4d
shkredder <108484392+shkredder@users.noreply.github.com>
2022-07-06 22:34:08 +0200

    Update main.js
    1. 5-й пункт исправлен.
    2. Поменяла отрицание (!)

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let userSurname = prompt("Enter Your surname").trim();
 //task 3
 let userEmail = prompt("Enter Your email").toLowerCase().replaceAll(" ", "");
 
-if (userEmail.includes("@") == false) {
+if (!userEmail.includes("@")) {
     email = `not valid email <b>${userEmail}</b> (symbol @ not exist)`;
 } else if (userEmail.startsWith("@")) {
     email = `not valid email <b>${userEmail}</b> (symbol @ find in first place)`;
@@ -23,6 +23,8 @@ let currYear = new Date().getFullYear();
 let age = currYear - userYearOfBirth;
 
 //result
-console.log(`Full name: ${userName} ${userSurname}
-Email: ${email}
-Age: ${age}`);
+document.write(`<ul class="userProfile">
+<li>Full name: ${userName} ${userSurname}</li>
+<li>Email: ${email}</li>
+<li>Age: ${age}</li>
+</ul>`);
